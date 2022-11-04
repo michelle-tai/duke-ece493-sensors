113adbeac1ed14d7b078bf283ef038c9832ed05a
Michelle Tai <michelle.tai@duke.edu>
2022-11-04 19:51:53 -0400

    detect ~2 ish second of no movement

--- maxsonar_mine/maxsonar_mine.ino
+++ maxsonar_mine/maxsonar_mine.ino
@@ -21,13 +21,14 @@
 //  Speed of Sound @ 22°C (71.6°F) = 344.5 m/S = 34450.0 cm/S
 //  0.0000290275 S/cm = 29.0275762 uS/cm = 73.73 uS/in
 #define SOUND_SPEED_uSpIN 73.73
-#define WINDOW_SIZE 12 //39 //58 //3÷0.250 = 12
-#define ZEROES_THRESHOLD 10 //30 // 46 
+//#define WINDOW_SIZE 12 //39 //58 //3÷0.250 = 12
+#define ZEROES_THRESHOLD 12 //30 // 46 
 #define LOOP_DELAY 250
 
 SoftwareSerial mseSerial(MSE_SDATA_PIN, 3, true); // RX,TX,inverse_logic (TX is not used)
 double prevMeasurement;
-double displaceArr[WINDOW_SIZE]; //58 since measurements taken ever 51ms, and around 58 of these in 3 sec
+double prevDisplacement;
+//double displaceArr[WINDOW_SIZE]; //58 since measurements taken ever 51ms, and around 58 of these in 3 sec
 int nearZeroCount; // threshold set to 46, which is ~80%. we can play with this later
 int winStart;
 int winEnd;
@@ -69,15 +70,45 @@ void sensorLoop(){
     }
 
     double displacement = pwDistance - prevMeasurement;
-    updateWindow(displacement);
-    if(nearZeroCount >= ZEROES_THRESHOLD){
-      Serial.print("[ ");
-      for(int i = 0; i < WINDOW_SIZE; i++){
-        Serial.print(displaceArr[i]);
-        Serial.print(" ");
+    if(prevDisplacement == NULL){
+      prevDisplacement = displacement;
+      continue;
+    }
+    
+    Serial.print("Displ:\t");
+    Serial.println(displacement);
+//    updateWindow(displacement);
+    Serial.println(abs(prevDisplacement));
+    Serial.println(abs(displacement));
+
+    if(abs(prevDisplacement) < 1){ // if prev was a 0
+      if(abs(displacement) < 1){ //if curr 0, then add to zero count
+        nearZeroCount++;
       }
-      Serial.print("]");
-      Serial.print("\n");
+      else{ //else if curr is not 0, then set the near zero count to = 0
+        nearZeroCount = 0;
+      }
+    }
+    else { // if prev was not 0
+      if(abs(displacement) < 1){ // and curr is 0, reset count to 1
+        nearZeroCount = 1;
+      }
+      else{ // curr is not 0 too
+        nearZeroCount = 0;
+      }
+    } 
+    Serial.println(nearZeroCount);
+
+
+
+    if(nearZeroCount >= ZEROES_THRESHOLD){
+//      Serial.print("[ ");
+//      for(int i = 0; i < WINDOW_SIZE; i++){
+//        Serial.print(displaceArr[i]);
+//        Serial.print(" ");
+//      }
+//      Serial.print("]");
+//      Serial.print("\n");
       Serial.println("FAILURE DETECTED");
       Serial.println(nearZeroCount);
       break;
@@ -86,53 +117,140 @@ void sensorLoop(){
       // break out of this loop 
     }
     prevMeasurement = pwDistance;
+    prevDisplacement = displacement;
     delay(LOOP_DELAY - MSE_READ_REQUIRED_DURATION_mS);
   }
   
 }
 
-void updateWindow(double displ){
-  Serial.print("displace: ");
-  Serial.println(displ);
-  if(winEnd < WINDOW_SIZE){
-    winEnd++;
-    displaceArr[winEnd] = displ;
-//    Serial.println("filling window");
-    if(displ >= -1.00 && displ <= 1.00){
-      nearZeroCount++;
-//      Serial.print("within threshold # entering unfilled window || count now: ");
-//      Serial.println(nearZeroCount);
-    }
-  }
-  else{
-    int winStartMod = winStart % WINDOW_SIZE;
-    double winStartVal = displaceArr[winStartMod];
-    if(winStartMod >= -1.00 && winStartMod <= 1.00 && nearZeroCount > 0){
-      nearZeroCount--;
+//void zeroCheck(){
+//  double pwDistance = triggerAndReadDistanceFromPulse();
+//  if(prevMeasurement == NULL){
+//    prevMeasurement = pwDistance;
+//    continue;
+//  }
+//  
+//  double displacement = pwDistance - prevMeasurement;
+//  // if see 0 & previous was 0
+//  if(abs(prevMeasurement) < 1){ // if prev was a 0
+//    if(abs(displacement) < 1){ //if curr 0, then add to zero count
+//      nearZeroCount++;
+//    }
+//    else{ //else if curr is not 0, then set the near zero count to = 0
+//      nearZeroCount = 0;
+//    }
+//  }
+//  else { // if prev was not 0
+//    if(abs(displacement) < 1){ // and curr is 0, reset count to 1
+//      nearZeroCount = 1;
+//    }
+//    else{ // curr is not 0 too
+//      nearZeroCount = 0;
+//    }
+//  }
+// 
+//}
+
+//void updateWindow(double displ){
+//  Serial.print("displace: ");
+//  Serial.println(displ);
+//
+//  // empty window and put in the displ as the first item in window
+//  if(abs(displaceArr[winEnd % WINDOW_SIZE]) < 1.00 && displ > 1.00){
+//    winStart = 0;
+//    winEnd = 0;
+//    displaceArr[winEnd] = displ;
+//    winEnd++;
+//    nearZeroCount = 0;
+//  }
+//  else{
+//    // filling window
+//    if(winEnd < WINDOW_SIZE){
+//      winEnd++;
+//      displaceArr[winEnd] = displ;
+//      //Serial.println("filling window");
+//      if(abs(displ) < 1.00){
+//        nearZeroCount++;
+//        //Serial.print("within threshold # entering unfilled window || count now: ");
+//        //Serial.println(nearZeroCount);
+//      }
+//    } 
+//    // window was already filled
+//    else {
+//      
+//    }
+//  }
+//  
+//
+//  //window already full?
+//  else{
+////    int winStartMod = winStart % WINDOW_SIZE;
+////    double winStartVal = displaceArr[winStartMod];
+////    double oldWinEndVal = displaceArr[winEnd % WINDOW_SIZE];
+//
+//    // replace the whole array when we see a zero to nonzero transition
+//    if(abs(displaceArr[winEnd % WINDOW_SIZE]) < 1.00 && displ > 1.00){
+//      winStart = 0;
+//      winEnd = 0;
+//      displaceArr[winEnd] = displ;
+//      winEnd++;
+//      nearZeroCount = 0;
+//    } 
+//    else {
+//      winStart++;
+//      winEnd++;
+//      
+//      int winEndMod = winEnd % WINDOW_SIZE;
+//      displaceArr[winEndMod] = displ;
+//      if(abs(oldWinEndVal) < 1.00 && displ > 1.00){
+//        winStart = 0;
+//        winEnd = 0;
+//        displaceArr[winEnd] = displ;
+//        winEnd++;
+//        nearZeroCount = 0;
+//      }
+//    }
+////    if(abs(winStartVal) < 1.00 && nearZeroCount > 0){
+////      nearZeroCount--;
+//////      Serial.print("within threshold # (");
+////    }
+//
+////    Serial.print(winStartVal);
+////    Serial.println(") leaving window");
+////    Serial.println(nearZeroCount);
+//    
+////    winStart++;
+////    winEnd++;
+////    
+////    int winEndMod = winEnd % WINDOW_SIZE;
+////    displaceArr[winEndMod] = displ;
+////    if(abs(oldWinEndVal) < 1.00 && displ > 1.00){
+////      winStart = 0;
+////      winEnd = 0;
+////      displaceArr[winEnd] = displ;
+////      winEnd++;
+////      nearZeroCount = 0;
+////    }
+//    else if(abs(displ) < 1.00){
+//      nearZeroCount++;
 //      Serial.print("within threshold # (");
-    }
-//    Serial.print(winStartVal);
-//    Serial.println(") leaving window");
-//    Serial.println(nearZeroCount);
-    
-    winStart++;
-    winEnd++;
-    
-    int winEndMod = winEnd % WINDOW_SIZE;
-    displaceArr[winEndMod] = displ;
-    if(displ >= -1.00 && displ <= 1.00){
-      nearZeroCount++;
-//      Serial.print("within threshold # (");
-//      Serial.print(displ);
-//      Serial.print(") entering window || count now: ");
-//      Serial.println(nearZeroCount);
-    }
-//    Serial.print(displ);
-//    Serial.println(") entering window");
-  }
-  Serial.print("count is now: ");
-  Serial.println(nearZeroCount);
-}
+////      Serial.print(displ);
+////      Serial.print(") entering window || count now: ");
+////      Serial.println(nearZeroCount);
+//    }
+////    if(displ >= -1.00 && displ <= 1.00){
+////      nearZeroCount++;
+//////      Serial.print("within threshold # (");
+//////      Serial.print(displ);
+//////      Serial.print(") entering window || count now: ");
+//////      Serial.println(nearZeroCount);
+////    }
+////    Serial.print(displ);
+////    Serial.println(") entering window");
+//  }
+//  Serial.print("count is now: ");
+//  Serial.println(nearZeroCount);
+//}
 
 /**
  * To assure that the distance pulse is detected going from low to high,
