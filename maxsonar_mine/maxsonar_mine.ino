5e1a79ce3e4e83514cba6ba0a6db80f258e2d891
Michelle Tai <michelle.tai@duke.edu>
2022-11-04 18:48:37 -0400

    works for # of 0s over a period of time, but need to figure out how to restart the count if not a near 0 displacement

--- maxsonar_mine/maxsonar_mine.ino
+++ maxsonar_mine/maxsonar_mine.ino
@@ -1,18 +1,12 @@
-/*
-not gonna use serial, use pulse since analog is only in .5s increments? and seems more reliable
-*/
-
+//not gonna use serial, use pulse since analog is only in .5s increments? and seems more reliable
 #include <SoftwareSerial.h>
 
 //   MaxSonar-EZ pins
 #define MSE_PW_PIN 5 //pw 
 #define MSE_READ_MODE_PIN 4 //rx
 #define MSE_SDATA_PIN 7 //tx but only used for chaining?
-
 //  GPIO Pins (analog)
 #define MSE_AN_PIN A0 //analog
-
-#define VCC 5
 
 // MaxSonar to Arduino control/adjustments
 #define MSE_READ_CONTINUOUS HIGH 
@@ -27,21 +21,23 @@ not gonna use serial, use pulse since analog is only in .5s increments? and seem
 //  Speed of Sound @ 22°C (71.6°F) = 344.5 m/S = 34450.0 cm/S
 //  0.0000290275 S/cm = 29.0275762 uS/cm = 73.73 uS/in
 #define SOUND_SPEED_uSpIN 73.73
-
-// scaling formula: [(Vcc/1024) = Vi] = volts per 5 mm
-/*
-- Outputs analog voltage with a scaling factor of (Vcc/512) per inch. A supply of 5V yields ~9.8mV/in. and
-3.3V yields ~6.4mV/in. The output is buffered and corresponds to the most recent range data.
-*/
-// range formula: [5*(Vm/Vi) = Ri]
+#define WINDOW_SIZE 12 //39 //58 //3÷0.250 = 12
+#define ZEROES_THRESHOLD 10 //30 // 46 
+#define LOOP_DELAY 250
 
 SoftwareSerial mseSerial(MSE_SDATA_PIN, 3, true); // RX,TX,inverse_logic (TX is not used)
-int _loopCount = 0; // Used to track the times through loop
-int _voltage_scale;
 double prevMeasurement;
-double displacementArr[58]; //58 since measurements taken ever 51ms, and aroudn 58 of these in 3 sec
+double displaceArr[WINDOW_SIZE]; //58 since measurements taken ever 51ms, and around 58 of these in 3 sec
+int nearZeroCount; // threshold set to 46, which is ~80%. we can play with this later
+int winStart;
+int winEnd;
+double winStartVal; 
+double winEndVal;
 
 void setup() {
+  nearZeroCount = 0;
+  winStart = 0;
+  winEnd = 0;
   // put your setup code here, to run once:
   Serial.begin(38400); // Open serial communications
   mseSerial.begin(9600); // Set the rate for MaxSonar communications
@@ -54,38 +50,88 @@ void setup() {
   pinMode(MSE_AN_PIN, INPUT);
   digitalWrite(MSE_READ_MODE_PIN, MSE_READ_IDLE); // Use 'triggered' mode
   pinMode(MSE_READ_MODE_PIN, OUTPUT);
-//  startMillis = millis();
-//  _voltage_scale = VCC / 1024; //0.009766V per inch
+  sensorLoop();
 }
 
 void loop() {
-// Trigger a measurement and read the distance using the pulse-width method
-  // then the serial and analog methods
-  unsigned long startMillis = millis();
-  double pwDistance = triggerAndReadDistanceFromPulse();
-  if (_loopCount++ % 20 == 0) {
-    Serial.println("Pulse:\tAnalog:\t"); 
-  }
-  Serial.print(pwDistance);
-  Serial.print("\t");
-  Serial.print(analogDistance);
-  Serial.println("\t");
+}
 
-  if(prevMeasurement == NULL){
+
+void sensorLoop(){
+  while(true){
+    double pwDistance = triggerAndReadDistanceFromPulse();
+    Serial.print("Pulse:\t");
+    Serial.println(pwDistance);
+  
+    if(prevMeasurement == NULL){
+      prevMeasurement = pwDistance;
+      continue;
+    }
+
+    double displacement = pwDistance - prevMeasurement;
+    updateWindow(displacement);
+    if(nearZeroCount >= ZEROES_THRESHOLD){
+      Serial.print("[ ");
+      for(int i = 0; i < WINDOW_SIZE; i++){
+        Serial.print(displaceArr[i]);
+        Serial.print(" ");
+      }
+      Serial.print("]");
+      Serial.print("\n");
+      Serial.println("FAILURE DETECTED");
+      Serial.println(nearZeroCount);
+      break;
+      // set certain pin to high
+      // print failure detected
+      // break out of this loop 
+    }
     prevMeasurement = pwDistance;
+    delay(LOOP_DELAY - MSE_READ_REQUIRED_DURATION_mS);
   }
-//  unsigned long endMill = millis();
+  
+}
 
-  double diff = pwDistance - prevMeasurement;
-  double diffTime = endMill - startMillis;
-  Serial.print(diff);
-  Serial.print("\t");
-  Serial.println(diffTime);
-  Serial.println(" ");
-  prevMeasurement = pwDistance;
-//  startMillis = endMill;
-//  **PRINT SOMETHING** 
-  delay(250 - MSE_READ_REQUIRED_DURATION_mS);
+void updateWindow(double displ){
+  Serial.print("displace: ");
+  Serial.println(displ);
+  if(winEnd < WINDOW_SIZE){
+    winEnd++;
+    displaceArr[winEnd] = displ;
+//    Serial.println("filling window");
+    if(displ >= -1.00 && displ <= 1.00){
+      nearZeroCount++;
+//      Serial.print("within threshold # entering unfilled window || count now: ");
+//      Serial.println(nearZeroCount);
+    }
+  }
+  else{
+    int winStartMod = winStart % WINDOW_SIZE;
+    double winStartVal = displaceArr[winStartMod];
+    if(winStartMod >= -1.00 && winStartMod <= 1.00 && nearZeroCount > 0){
+      nearZeroCount--;
+//      Serial.print("within threshold # (");
+    }
+//    Serial.print(winStartVal);
+//    Serial.println(") leaving window");
+//    Serial.println(nearZeroCount);
+    
+    winStart++;
+    winEnd++;
+    
+    int winEndMod = winEnd % WINDOW_SIZE;
+    displaceArr[winEndMod] = displ;
+    if(displ >= -1.00 && displ <= 1.00){
+      nearZeroCount++;
+//      Serial.print("within threshold # (");
+//      Serial.print(displ);
+//      Serial.print(") entering window || count now: ");
+//      Serial.println(nearZeroCount);
+    }
+//    Serial.print(displ);
+//    Serial.println(") entering window");
+  }
+  Serial.print("count is now: ");
+  Serial.println(nearZeroCount);
 }
 
 /**
@@ -98,10 +144,12 @@ void loop() {
  * Refer to the Timing Diagram and Description in the datasheet for timing details.
  */
 double triggerAndReadDistanceFromPulse() {
+  mseSerial.stopListening();
+  mseSerial.listen();
   // Assure that the trigger is set to IDLE (HOLD)
   digitalWrite(MSE_READ_MODE_PIN, MSE_READ_IDLE);
   delayMicroseconds(10);
-  //
+
   // Minimum time for readings (start of reading to start of reading)
   // is 49mS.
   //
